e9f7dfe6125c51643ce74849ab15069b8347c00f
sarveshkore <sarvesh2k2@gmail.com>
2024-05-31 18:20:15 +0530

    rendering cards using config

--- src/Components/Cardcontainer.js
+++ src/Components/Cardcontainer.js
@@ -1,178 +1,174 @@
 import Restaurantcard from "./Restaurantcart";
+// import {config} from "../const/config";
+import { restaurantList } from "../const/config";
+import { IMG_URL } from "../const/config";
 const Cardcontainer=()=>{
-      const restaurantData=[
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597",
-          title:"Chinese Yok",
-          starRating:"⭐4.8",
-          deliveryTime:"40-45",
-          cuisins:"Chinese, Italian",
-          location:"Ghatkopar W"  
-        },
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7",
-          title:"Pizza Hut",
-          starRating:"⭐4.5",
-          deliveryTime:"25-30",
-          cuisins:"Pizza",
-          location:"BKC" 
-        },
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rmlcifdqtpoxdrdvet2s",
-          title:"Achija Veg Restaurant",
-          starRating:"⭐4.9",
-          deliveryTime:"30-35",
-          cuisins:"South Indian",
-          location:"Vidyavihar"  
-        },
+      // const restaurantData=[
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597",
+      //     title:"Chinese Yok",
+      //     starRating:"⭐4.8",
+      //     deliveryTime:"40-45",
+      //     cuisins:"Chinese, Italian",
+      //     location:"Ghatkopar W"  
+      //   },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7",
+      //     title:"Pizza Hut",
+      //     starRating:"⭐4.5",
+      //     deliveryTime:"25-30",
+      //     cuisins:"Pizza",
+      //     location:"BKC" 
+      //   },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rmlcifdqtpoxdrdvet2s",
+      //     title:"Achija Veg Restaurant",
+      //     starRating:"⭐4.9",
+      //     deliveryTime:"30-35",
+      //     cuisins:"South Indian",
+      //     location:"Vidyavihar"  
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/o5bbufmcjoqdfvtm7r1n",
-          title:"Rangoon Zaika",
-          starRating:"⭐4.7",
-          deliveryTime:"25-30",
-          cuisins:"Indian, Chinese",
-          location:"Chinchpokli" 
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/o5bbufmcjoqdfvtm7r1n",
+      //     title:"Rangoon Zaika",
+      //     starRating:"⭐4.7",
+      //     deliveryTime:"25-30",
+      //     cuisins:"Indian, Chinese",
+      //     location:"Chinchpokli" 
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/df600a304a4c328795e659b072d93208",
-          title:"Mughal Dhaba",
-          starRating:"⭐4.3",
-          deliveryTime:"40-45",
-          cuisins:"Chinese",
-          location:"Kanjurmarg"
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/df600a304a4c328795e659b072d93208",
+      //     title:"Mughal Dhaba",
+      //     starRating:"⭐4.3",
+      //     deliveryTime:"40-45",
+      //     cuisins:"Chinese",
+      //     location:"Kanjurmarg"
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/230485dfde0c4916567efc8cf6ccae5e",
-          title:"Quick Combos@69",
-          starRating:"⭐4.9",
-          deliveryTime:"45-50",
-          cuisins:"North Indian, Thali",
-          location:"Marol" 
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/230485dfde0c4916567efc8cf6ccae5e",
+      //     title:"Quick Combos@69",
+      //     starRating:"⭐4.9",
+      //     deliveryTime:"45-50",
+      //     cuisins:"North Indian, Thali",
+      //     location:"Marol" 
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ys88tnnklqgfhzsftmqq",
-          title:"Baba Falooda",
-          starRating:"⭐4.5",
-          deliveryTime:"30-35",
-          cuisins:"Ice Cream, Beverages",
-          location:"" 
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ys88tnnklqgfhzsftmqq",
+      //     title:"Baba Falooda",
+      //     starRating:"⭐4.5",
+      //     deliveryTime:"30-35",
+      //     cuisins:"Ice Cream, Beverages",
+      //     location:"" 
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/0766b54629275731447e3d64740befb2",
-          title:"Tibb's Frankie",
-          starRating:"⭐4.7",
-          deliveryTime:"30-40",
-          cuisins:"Fast Food",
-          location:"Kanjurmarg" 
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/0766b54629275731447e3d64740befb2",
+      //     title:"Tibb's Frankie",
+      //     starRating:"⭐4.7",
+      //     deliveryTime:"30-40",
+      //     cuisins:"Fast Food",
+      //     location:"Kanjurmarg" 
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597",
-          title:"Chinese Yok",
-          starRating:"⭐4.8",
-          deliveryTime:"40-45",
-          cuisins:"Chinese, Italian",
-          location:"Ghatkopar W"  
-        },
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7",
-          title:"Pizza Hut",
-          starRating:"⭐4.5",
-          deliveryTime:"25-30",
-          cuisins:"Pizza",
-          location:"BKC" 
-        },
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rmlcifdqtpoxdrdvet2s",
-          title:"Achija Veg Restaurant",
-          starRating:"⭐4.9",
-          deliveryTime:"30-35",
-          cuisins:"South Indian",
-          location:"Vidyavihar" 
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597",
+      //     title:"Chinese Yok",
+      //     starRating:"⭐4.8",
+      //     deliveryTime:"40-45",
+      //     cuisins:"Chinese, Italian",
+      //     location:"Ghatkopar W"  
+      //   },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7",
+      //     title:"Pizza Hut",
+      //     starRating:"⭐4.5",
+      //     deliveryTime:"25-30",
+      //     cuisins:"Pizza",
+      //     location:"BKC" 
+      //   },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/rmlcifdqtpoxdrdvet2s",
+      //     title:"Achija Veg Restaurant",
+      //     starRating:"⭐4.9",
+      //     deliveryTime:"30-35",
+      //     cuisins:"South Indian",
+      //     location:"Vidyavihar" 
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/o5bbufmcjoqdfvtm7r1n",
-          title:"Rangoon Zaika",
-          starRating:"⭐4.7",
-          deliveryTime:"25-30",
-          cuisins:"Indian, Chinese",
-          location:"Chinchpokli" 
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/o5bbufmcjoqdfvtm7r1n",
+      //     title:"Rangoon Zaika",
+      //     starRating:"⭐4.7",
+      //     deliveryTime:"25-30",
+      //     cuisins:"Indian, Chinese",
+      //     location:"Chinchpokli" 
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/df600a304a4c328795e659b072d93208",
-          title:"Mughal Dhaba",
-          starRating:"⭐4.3",
-          deliveryTime:"40-45",
-          cuisins:"Chinese",
-          location:"Kanjurmarg"
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/df600a304a4c328795e659b072d93208",
+      //     title:"Mughal Dhaba",
+      //     starRating:"⭐4.3",
+      //     deliveryTime:"40-45",
+      //     cuisins:"Chinese",
+      //     location:"Kanjurmarg"
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/230485dfde0c4916567efc8cf6ccae5e",
-          title:"Quick Combos@69",
-          starRating:"⭐4.9",
-          deliveryTime:"45-50",
-          cuisins:"North Indian, Thali",
-          location:"Marol" 
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/230485dfde0c4916567efc8cf6ccae5e",
+      //     title:"Quick Combos@69",
+      //     starRating:"⭐4.9",
+      //     deliveryTime:"45-50",
+      //     cuisins:"North Indian, Thali",
+      //     location:"Marol" 
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ys88tnnklqgfhzsftmqq",
-          title:"Baba Falooda",
-          starRating:"⭐4.5",
-          deliveryTime:"30-35",
-          cuisins:"Ice Cream, Beverages",
-          location:"" 
-        },
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ys88tnnklqgfhzsftmqq",
+      //     title:"Baba Falooda",
+      //     starRating:"⭐4.5",
+      //     deliveryTime:"30-35",
+      //     cuisins:"Ice Cream, Beverages",
+      //     location:"" 
+      //   },
 
-        {
-          imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/0766b54629275731447e3d64740befb2",
-          title:"Tibb's Frankie",
-          starRating:"⭐4.7",
-          deliveryTime:"30-40",
-          cuisins:"Fast Food",
-          location:"Kanjurmarg" 
-        }
+      //   {
+      //     imgUrl:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/0766b54629275731447e3d64740befb2",
+      //     title:"Tibb's Frankie",
+      //     starRating:"⭐4.7",
+      //     deliveryTime:"30-40",
+      //     cuisins:"Fast Food",
+      //     location:"Kanjurmarg" 
+      //   }
 
-
-
-
-        
-      ]
-
+      // ]
+      console.log("restaurantList");
+      const restaurants=restaurantList[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     return(
       // <div>This is cardcontainer component</div>
       <div className="container d-flex flex-wrap  gap-4">
-
         {
-          restaurantData.map((restaurant)=>{
+          restaurants.map((restaurant)=>{
             return(
-              <Restaurantcard
-                // {...restaurant}     
-                {...restaurant}     
 
-              // imgUrl={restaurant?.imageUrl}
-                // title={restaurant?.title}
-                // starRating={restaurant?.starRating}
-                // deliveryTime={restaurant?.deliveryTime}
-                // cuisins={restaurant?.cuisins}
-                // location={restaurant?.location}
-    
-              
-              />
-            );
+<Restaurantcard
+        // imageUrl={IMG_URL+restaurant?.info?.cloudinaryImageId}
+        // title={restaurant?.info?.name}
+        // starRating={restaurant?.info?.avgRating}
+        // deliveryTime={restaurant?.info?.sla?.deliveryTime}
+        // cuisins={restaurant?.info?.cuisines.join(", ")}
+        // location={restaurant?.info?.areaName}/>
+              {...restaurant?.info}/>
+
+            )
           })
-            
-          
-        }
 
+        
+        }
+        
         {/* <Restaurantcard/> */}
       </div>
       
--- src/Components/Restaurantcart.js
+++ src/Components/Restaurantcart.js
@@ -1,4 +1,9 @@
-const Restaurantcard=({imgUrl,title,starRating,deliveryTime,cuisins,location})=>{
+import { IMG_URL } from "../const/config";
+
+
+// const Restaurantcard=({imgUrl,title,starRating,deliveryTime,cuisins,location})=>{
+const Restaurantcard=({cloudinaryImageId,name,avgRating,sla,cuisines,areaName})=>{
+  
   // imgUrl,title,starRating,deliveryTime,cuisins,location
   // console.log("props",props);
   // const =props;
@@ -6,16 +11,22 @@ const Restaurantcard=({imgUrl,title,starRating,deliveryTime,cuisins,location})=>
       <div className="custom-card " >
         <div className="mb-2 rounded-4">
           <img 
-          src={imgUrl}        // src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"
+          src={IMG_URL+cloudinaryImageId}        // src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"
+          //src={imgUrl}        // src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"
           /></div>
         <div className="px-2">
-          <h5>{title}</h5>        {/* <h5>Chinese Yok</h5> */}
+          <h5>{name}</h5>        {/* <h5>Chinese Yok</h5> */}
+          {/* <h5>{title}</h5>        <h5>Chinese Yok</h5> */}
           <div className="d-flex justify-content-between">
-            <div>{starRating}</div>       {/* <div>⭐4.5</div> */}
-            <div>{deliveryTime}min</div>        {/* <div>40-45 min</div> */}
+            <div>{avgRating}</div>       {/* <div>⭐4.5</div> */}
+            {/* <div>{starRating}</div>       <div>⭐4.5</div> */}
+            <div>{sla?.deliveryTime}min</div>        {/* <div>40-45 min</div> */}
+            {/* <div>{deliveryTime}min</div>        <div>40-45 min</div> */}
           </div>
-          <div className="text-secondary">{cuisins}</div>       {/* <div className="text-secondary">Chinese, Italian</div> */}
-          <div>{location}</div>       {/* <div>Ghatkopar W</div> */}
+          <div className="text-secondary">{cuisines.join(", ")}</div>       {/* <div className="text-secondary">Chinese, Italian</div> */}
+          {/* <div className="text-secondary">{cuisins}</div>       <div className="text-secondary">Chinese, Italian</div> */}
+          <div>{areaName}</div>       {/* <div>Ghatkopar W</div> */}
+          {/* <div>{location}</div>       <div>Ghatkopar W</div> */}
         </div>
 
       </div>
